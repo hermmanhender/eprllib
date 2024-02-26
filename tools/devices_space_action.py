aab8f8e02f1dce65f38aa67ca8adb8b49c612f14
hermmanhender <hendger.07@gmail.com>
2024-02-26 10:14:12 +0100

    Update devices_space_action.py
    Action space of DualSetPoint thermostats was added.

--- tools/devices_space_action.py
+++ tools/devices_space_action.py
@@ -1,48 +1,16 @@
 """Action spaces for diferent agents that operate devices in a centralized way.
 """
 
-def natural_ventilation_action(central_action: int):
-    """_summary_
-
-    Args:
-        central_action (int): _description_
-
-    Returns:
-        _type_: _description_
-    """
-    action_space = [
-        [0,0],
-        [0,1],
-        [1,0],
-        [1,1]
-    ]
-    return action_space[central_action]
-
-def natural_ventilation_central_action(action1: int, action2: int):
-    """_summary_
-
-    Args:
-        action1 (int): _description_
-        action2 (int): _description_
-
-    Returns:
-        _type_: _description_
-    """
-    action_space = [
-        [0,0],
-        [0,1],
-        [1,0],
-        [1,1]
-    ]
-    index = 0
-    for a in action_space:
-        if a == [action1, action2]:
-            central_action = index
-            break
-        else:
-            index += 1
+class DualSetPoint:
+    def __init__(self):
+        self.action_space = []
+        for n in range(17,28,1):
+            for m in range(18,29,1):
+                if m > n:
+                    self.action_space.append([n,m])
     
-    return central_action
+    def dual_action(self, central_action: int):
+        return self.action_space[central_action]
 
 class TwoWindowsCentralizedControl:
     def __init__(self):
