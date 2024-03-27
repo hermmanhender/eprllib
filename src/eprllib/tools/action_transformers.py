ce9f5d4b30f1c1664c8972f15146e271b08244ac
hermmanhender <52546433+hermmanhender@users.noreply.github.com>
2024-03-27 11:23:41 +0100

    Athermostat_dual upgrade
    agent_id was added and also the function and action space was implemented.

--- src/eprllib/tools/action_transformers.py
+++ src/eprllib/tools/action_transformers.py
@@ -3,8 +3,16 @@ eprllib. Most of them are applied in the test section where examples to test the
 library are developed.
 """
 
-def thermostat_dual(action):
-    """This method take a discret action in the range of [0,xx) and transform it
-    into a temperature tuple of cooling and heating setpoints, respectively.
+def thermostat_dual(agent_id, action):
+    """This method take a discret action in the range of [0,4) and transforms it
+    into a temperature of cooling or heating setpoints, depending the agent id 
+    involve.
     """
-    return cooling_action, heating_action
+    if agent_id == 'cooling_setpoint':
+        transform_action = 23 + action
+    elif agent_id == 'heating_setpoint':
+        transform_action = 21 - 
+    else:
+        transform_action = -1
+        print('The agent id it is not in the list of agents for this action_transform_method. The agent allowed are cooling_setpoint and heating_setpoint. Please notice that.')
+    return transform_action
