625303556015b5b6606bcdef029c5785936aa0fb
hermmanhender <hendger.07@gmail.com>
2024-03-27 16:25:53 +0100

    Update rewards.py
    Correct default values and add a new method for the use in an example.

--- src/eprllib/tools/rewards.py
+++ src/eprllib/tools/rewards.py
@@ -16,7 +16,7 @@ def reward_function_T3(config: Dict[str, Any], obs: dict, infos: dict) -> float:
         float: reward value
     """
     agent_ids = config['agent_ids']
-    T_confort = config.get('T_confort', 23.5)
+    T_confort = config.get('T_confort', 22)
     occupancy = infos[agent_ids[0]]['occupancy']
     T_zone = infos[agent_ids[0]]['Ti']
     if occupancy > 0: # When there are people in the thermal zone, a reward is calculated.
@@ -48,7 +48,7 @@ def reward_function_T2(config: Dict[str, Any], obs: dict, infos: dict) -> float:
         float: reward value
     """
     agent_ids = config['agent_ids']
-    T_confort = config.get('T_confort', 23.5)
+    T_confort = config.get('T_confort', 22)
     occupancy = infos[agent_ids[0]]['occupancy']
     T_zone = infos[agent_ids[0]]['Ti']
     if occupancy > 0: # When there are people in the thermal zone, a reward is calculated.
@@ -63,4 +63,41 @@ def reward_function_T2(config: Dict[str, Any], obs: dict, infos: dict) -> float:
             reward = -49.
         else:
             reward = 0.
+    return reward
+
+def reward_function_T3_Energy(config: Dict[str, Any], obs: dict, infos: dict) -> float:
+    """This function returns the reward calcualted as the absolute value of the cube in the 
+    difference between set point temperatur for comfort and the temperature measured in the 
+    thermal zone when there are people in the zone but zero when is not.
+
+    Args:
+        config (Dict[str, Any]): env_config dictionary. Optionaly you can configurate the 'T_confot' variable.
+        obs (dict): Zone Mean Air Temperature for the Thermal Zone in °C.
+        infos (dict): infos dict must to provide the occupancy level and the Zone Mean Temperature.
+        agent_ids (list): list of agent acting in the environment.
+
+    Returns:
+        float: reward value
+    """
+    agent_ids = config['agent_ids']
+    
+    beta_reward = config.get('beta_reward', 0.5)
+    T_confort = config.get('T_confort', 22)
+    occupancy = infos[agent_ids[0]]['occupancy']
+    T_zone = infos[agent_ids[0]]['Ti']
+    cooling_meter = infos[agent_ids[0]]['cooling_meter']
+    heating_meter = infos[agent_ids[0]]['heating_meter']
+    
+    if occupancy > 0: # When there are people in the thermal zone, a reward is calculated.
+        reward = -beta_reward*(cooling_meter+heating_meter) -(1-beta_reward)*(min(abs((T_confort - T_zone)**3),343.))
+    else:
+        # If there are not people, only the reward is calculated when the environment is far away
+        # from the comfort temperature ranges. This limits are recommended in EnergyPlus documentation:
+        # InputOutput Reference p.522
+        if T_zone > 29.4:
+            reward = -beta_reward*(cooling_meter+heating_meter) -(1-beta_reward)*343.
+        elif T_zone < 16.7:
+            reward = -beta_reward*(cooling_meter+heating_meter) -(1-beta_reward)*343.
+        else:
+            reward = -beta_reward*(cooling_meter+heating_meter)
     return reward
