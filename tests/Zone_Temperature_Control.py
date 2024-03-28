40fda12b921a7e151ff387e786dc9f610bc8b178
hermmanhender <52546433+hermmanhender@users.noreply.github.com>
2024-03-28 14:00:42 +0100

    Update the description in Zone_Temperature_Control.py

--- tests/Zone_Temperature_Control.py
+++ tests/Zone_Temperature_Control.py
@@ -4,28 +4,41 @@
 ## Problem Statement
 
 Many models have schedule inputs that could be used to control the object, 
-but creating the schedules is often cumbersome and may not always result in 
-optimal behavior due to dynamic environmental factors. Leveraging schedules 
-as inputs for Deep Reinforcement Learning (DRL) offers a promising approach 
-to learning optimal control policies. In this example, we demonstrate how 
-DRL can be applied to learn the optimal policy for setting heating and 
-cooling zone temperature schedules.
+but creating the schedules is often cumbersome and may not always result in optimal behavior 
+due to dynamic environmental factors. Leveraging schedules as inputs for 
+Deep Reinforcement Learning (DRL) offers a promising approach to learning 
+optimal control policies. In this example, we demonstrate how DRL can be 
+applied to learn the optimal policy for setting heating and cooling zone 
+temperature schedules.
 
 ## DRL Design Discussion
 
-As an example, we will utilize the model of the Small Office Reference 
-Building (RefBldgSmallOfficeNew2004_Chicago.idf) and utilize the 
-EnergyPlus API Python to optimize heating and cooling zone temperature 
-setpoint schedules. The input object `Schedule:Constant` has been configured 
-to serve as an actuator (or agent within the scope of eprllib).
+As an example, we will utilize 
+the model of the Small Office Reference Building 
+(RefBldgSmallOfficeNew2004_Chicago.idf) and utilize the EnergyPlus API 
+Python to optimize heating and cooling zone temperature setpoint schedules. 
+Instead of modifying existing schedules, we will overwrite them using the 
+zone actuator "Zone Temperature Control" and the keys "Heating Setpoint" 
+and "Cooling Setpoint". Two agents will be applied with a DQN learning 
+method and fully shared parameters.
 
-To facilitate the DRL process, we must explicitly define the action space, 
-detailing the possible actions the agent can take in adjusting the temperature 
-setpoints. Additionally, we need to specify the variables comprising the 
-observation space, providing clarity on the information available to the 
-agent at each time step. For the reward function, we define a range of 
-temperatures within which we aim to maintain the environment to ensure 
-comfort for the building occupants.
+To facilitate the DRL process, we must 
+explicitly define the action space, detailing the possible actions the 
+agents can take in adjusting the temperature setpoints. Additionally, we 
+need to specify the variables comprising the observation space, providing 
+clarity on the information available to the agents at each time step. For 
+the reward function, it is defined as follows: 
+
+[ \text{reward} = -\beta \times (\text{heating_{energy}} + \text{cooling_{energy}}) - (1 - \beta) \times \text{PPD} ] 
+
+Where:
+    * (\beta) is the parameter used to balance between energy demand and comfort.
+    * (\text{heating_{energy}}) and (\text{cooling_{energy}}) represent the amount of energy used for heating and cooling, respectively, in each time step.
+    * (\text{PPD}) denotes the predicted percentage of discomfort in the time step, calculated using the Fanger (1970) comfort model.
+    
+This reward function effectively balances energy consumption with occupant 
+comfort, providing a comprehensive metric for evaluating the performance of 
+the control policies.
 
 Furthermore, it is essential to elucidate how the DRL algorithm interacts 
 with the EnergyPlus simulation. By detailing the learning process over 
