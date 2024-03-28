7a933d37a971ca2adff7a40f18172258bfb15c21
hermmanhender <52546433+hermmanhender@users.noreply.github.com>
2024-03-28 14:06:35 +0100

    Actualizar conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'eprllib'
 copyright = '2024, Germán Rodolfo Henderson'
 author = 'Germán Rodolfo Henderson'
-release = '1'
+release = '1.0.7'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
