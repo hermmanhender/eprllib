153773808bf2fb6cab8afc89d8dd564d85aee7fe
hermmanhender <hendger.07@gmail.com>
2024-05-03 16:38:11 +0200

    Upgrade evaluation config

--- src/eprllib/postprocess/marl_init_noventilation.py
+++ src/eprllib/postprocess/marl_init_noventilation.py
@@ -6,8 +6,6 @@ import sys
 sys.path.insert(0, 'C:/Users/grhen/Documents/GitHub/natural_ventilation_EP_RLlib')
 import csv
 from eprllib.env.multiagent.marl_ep_gym_env import EnergyPlusEnv_v0
-from eprllib.agents.conventional import Conventional
-
 
 def init_rb_evaluation(
     env_config: dict,
@@ -120,35 +118,65 @@ if __name__ == '__main__':
     
     import gymnasium as gym
     import os
-    from tools import rewards
+    from eprllib.tools import rewards
     
     name = 'prot3_novent'
     
-    env_config={ 
-        'weather_folder': 'C:/Users/grhen/Documents/GitHub/natural_ventilation_EP_RLlib/epw/GEF',
+    env_config={
+        # === ENERGYPLUS OPTIONS === #
+        'epjson': "/src/eprllib/epjson/prot_3_ceiling.epJSON",
+        "epw": "/src/eprllib/epw/GEF/GEF_Lujan_de_cuyo-hour-H4.epw",
         'output': 'C:/Users/grhen/Documents/Resultados_RLforEP/'+ name,
-        'epjson_folderpath': 'C:/Users/grhen/Documents/GitHub/natural_ventilation_EP_RLlib/epjson',
-        'epjson_output_folder': 'C:/Users/grhen/Documents/models',
-        # Configure the directories for the experiment.
         'ep_terminal_output': True,
-        # For dubugging is better to print in the terminal the outputs of the EnergyPlus simulation process.
+
+        # === EXPERIMENT OPTIONS === #
         'is_test': True,
-        # For evaluation process 'is_test=True' and for trainig False.
-        'test_init_day': 1,
-        'action_space': gym.spaces.Discrete(4),
-        # action space for simple agent case
-        'observation_space': gym.spaces.Box(float("-inf"), float("inf"), (307,)),
-        # observation space for simple agent case
         
-        # BUILDING CONFIGURATION
-        'building_name': 'prot_3_ceiling',
-        'reward_function': rewards.reward_function,
+        # === ENVIRONMENT OPTIONS === #
+        'action_space': gym.spaces.Discrete(2),
+        'reward_function': rewards.reward_function_T3,
+        "ep_variables":{
+            "To": ("Site Outdoor Air Drybulb Temperature", "Environment"),
+            "Ti": ("Zone Mean Air Temperature", "Thermal Zone: Living"),
+            "v": ("Site Wind Speed", "Environment"),
+            "d": ("Site Wind Direction", "Environment"),
+            "RHo": ("Site Outdoor Air Relative Humidity", "Environment"),
+            "RHi": ("Zone Air Relative Humidity", "Thermal Zone: Living"),
+            "pres": ("Site Outdoor Air Barometric Pressure", "Environment"),
+            "occupancy": ("Zone People Occupant Count", "Thermal Zone: Living"),
+            "ppd": ("Zone Thermal Comfort Fanger Model PPD", "Living Occupancy")
+        },
+        "ep_meters": {
+            "electricity": "Electricity:Zone:THERMAL ZONE: LIVING",
+            "gas": "NaturalGas:Zone:THERMAL ZONE: LIVING",
+        },
+        "ep_actuators": {
+            "opening_window_1": ("AirFlow Network Window/Door Opening", "Venting Opening Factor", "living_NW_window"),
+            "opening_window_2": ("AirFlow Network Window/Door Opening", "Venting Opening Factor", "living_E_window"),
+        },
+        'time_variables': [
+            'hour',
+            'day_of_year',
+            'day_of_the_week',
+            ],
+        'weather_variables': [
+            'is_raining',
+            'sun_is_up',
+            "today_weather_beam_solar_at_time",
+            ],
+        "infos_variables": ["ppd", "occupancy", "Ti"],
+        "no_observable_variables": ["ppd"],
+        
+        # === OPTIONAL === #
+        "timeout": 10,
+        "T_confort": 23.5,
+        "weather_prob_days": 2
     }
     
     policy_config = { # configuracion del control convencional
-        'SP_temp': 22, #es el valor de temperatura de confort
-        'dT_up': 2.5, #es el límite superior para el rango de confort
-        'dT_dn': 2.5, #es el límite inferior para el rango de confort
+        'SP_temp': 23.5, #es el valor de temperatura de confort
+        'dT_up': 3.5, #es el límite superior para el rango de confort
+        'dT_dn': 3.5, #es el límite inferior para el rango de confort
     }
     try:
         os.makedirs(env_config['output'])
