ee15bf4b39626a91bdc5a99f55cb60166e2121fa
hermmanhender <hendger.07@gmail.com>
2024-05-17 21:28:39 +0200

    Upgrade version to 1.1.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'eprllib'
 copyright = '2024, Germán Rodolfo Henderson'
 author = 'Germán Rodolfo Henderson'
-release = '1.1.1'
+release = '1.1.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
