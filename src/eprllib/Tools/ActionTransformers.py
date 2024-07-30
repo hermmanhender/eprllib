c368605aaea5d06294633d298e772bcfcfcc6cdf
hermmanhender <hendger.07@gmail.com>
2024-07-30 14:26:52 +0200

    Update ActionTransformers.py

--- src/eprllib/Tools/ActionTransformers.py
+++ src/eprllib/Tools/ActionTransformers.py
@@ -22,16 +22,19 @@ class DualSetPointThermostat(ActionTransformer):
     def __init__(self, agents_config:Dict, _agent_ids:Set):
         super().__init__(agents_config, _agent_ids)
     
-    def transform_action(self, action:Dict[str,float]) -> Dict[str, Any]:
+    def transform_action(self, action:Dict[str,int]) -> Dict[str, float|int]:
+        action_transformed = {agent: 0. for agent in self._agent_ids}
         for agent in self._agent_ids:
             actuator_type = self.agents_config[agent]['actuator_type']
             if actuator_type == 1: # Cooling
-                # transfort value between [0,1] to [16,27]
-                action[agent] = 16 + action[agent]*11
+                # transfort value between [0,1] to [23,27]
+                action_transformed[agent] = 23 + (action[agent]/10)*(27-23)
             elif actuator_type == 2: # Heating
-                # transfort value between [0,1] to [18,25]
-                action[agent] = 18 + action[agent]*7
+                # transfort value between [0,1] to [18,22]
+                action_transformed[agent] = 18 + (action[agent]/10)*(22-18)
+            elif actuator_type == 3: # Flow Rate
+                action_transformed[agent] = action[agent]/20
             else:
                 raise ValueError('Actuator type not valid.')
-            
-        return action
+        
+        return action_transformed
