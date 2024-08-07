78647514b3c7843d81f86ef5edf7498c39cf10c2
Germán <52546433+hermmanhender@users.noreply.github.com>
2024-08-08 01:13:00 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'eprllib'
 copyright = '2024, Germán Rodolfo Henderson'
 author = 'Germán Rodolfo Henderson'
-release = 'v1.3.2'
+release = 'v1.3.3'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
