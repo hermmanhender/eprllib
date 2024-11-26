609ee1f25a647d48a8f859dd256928e7ae2fddca
hermmanhender <hendger.07@gmail.com>
2024-11-26 14:49:14 +0100

    Update EnvConfig.py

--- src/eprllib/Env/EnvConfig.py
+++ src/eprllib/Env/EnvConfig.py
@@ -5,14 +5,11 @@ Environment Configuration
 This module contain the class and methods used to configure the environment.
 """
 
-from typing import Optional, List, Dict, Tuple, Any, TypeVar
-from gymnasium import spaces
+from typing import Optional, List, Dict, Tuple, Any
 from eprllib.ActionFunctions.ActionFunctions import ActionFunction
 from eprllib.RewardFunctions.RewardFunctions import RewardFunction
 from eprllib.EpisodeFunctions.EpisodeFunctions import EpisodeFunction
 from eprllib.ObservationFunctions.ObservationFunctions import ObservationFunction
-
-ActType = TypeVar("ActType")
 
 def env_config_to_dict(EnvConfig) -> Dict:
     """
@@ -24,6 +21,182 @@ class EnvConfig:
     def __init__(self):
         """
         This is the main object that it is used to relate the EnergyPlus model and the RLlib policy training execution.
+        
+        **generals**
+        
+        * epjson_path (str): Path to the IDF/epJSON file. If you pretend to modify the EnergyPlus model with `eprllib.EpisodeFunctions`
+        it is recommended to use the epJSON format.
+        * epw_path (str): Path to the EnergyPlus Weather (epw) file.
+        * output_path (str): Path to the output directory. If not specified, the default is the current directory.
+        * ep_terminal_output (bool): Indicate if the EnergyPlus outputs during simulation must print or not in the Terminal. Default is True.
+        * timeout (float): Time to wait for an observation of the environment. If you habe a slow model, you can increase the value of 
+        this parameter. Default is 10.0
+
+        **agents**
+        
+        Actuators/agents are the way that users modify the program at runtime using custom logic
+        and calculations. Not every variable inside EnergyPlus can be actuated. This is intentional,
+        because opening that door could allow the program to run at unrealistic conditions, with flow
+        imbalances or energy imbalances, and many other possible problems. Instead, a specific set of
+        items are available to actuate, primarily control functions, flow requests, and environmental
+        boundary conditions. These actuators, when used in conjunction with the runtime API and
+        data exchange variables, allow a user to read data, make decisions and perform calculations,
+        then actuate control strategies for subsequent time steps.
+        Actuator functions are similar, but not exactly the same, as for variables. An actuator
+        handle/ID is still looked up, but it takes the actuator type, component name, and control
+        type, since components may have more than one control type available for actuation. The
+        actuator can then be “actuated” by calling a set-value function, which overrides an internal
+        value, and informs EnergyPlus that this value is currently being externally controlled. To
+        allow EnergyPlus to resume controlling that value, there is an actuator reset function as well.
+        
+        * agents_config (Dict[str,Dict[str,Any]]):
+        
+
+        **observations**
+        
+        * observation_fn (ObservationFunction): 
+        * observation_fn_config (Dict[str, Any]): Deafult is an emptly dict.
+        
+        Variables represent time series output variables in the simulation. There are thousands
+        of variables made available based on the specific configuration. A user typically requests
+        variables to be in their output files by adding Output:Variable objects to the input file. It
+        is important to note that if the user does not request these variables, they are not tracked,
+        and thus not available on the API.
+        
+        * variables_env: List = []
+        * variables_thz: List = []
+        * variables_obj: Dict[str,Tuple[str,str]] = {} # {'agent_ID':('variable','key_object')}
+        
+        Meters represent groups of variables which are collected together, much like a meter on
+        a building which represents multiple energy sources. Meters are handled the same way as
+        variables, except that meters do not need to be requested prior running a simulation. From
+        an API standpoint, a client must simply get a handle to a meter by name, and then access
+        the meter value by using a get-value function on the API.
+        
+        * meters: Dict[str,Tuple[str,str]] = {} # {'agent_ID':('variable','key_object')}
+        
+        The name “internal variable” is used here as it is what these variables were
+        called in the original EMS implementation. Another name for these variables could be “static”
+        variables. Basically, these variables represent data that does not change throughout a simulation 
+        period. Examples include calculated zone volume or autosized equipment values. These
+        values are treated just like meters, you use one function to access a handle ID, and then use
+        this handle to lookup the value.
+        
+        * static_variables: Dict[str,Tuple[str,str]] = {} # {'thermal_zone_ID':('variable','key_object')}
+        
+        
+        A number of parameters are made available as they vary through the
+        simulation, including the current simulation day of week, day of year, hour, and many other
+        things. These do not require a handle, but are available through direct function calls.
+        
+        * simulation_parameters: Dict[str,bool] = {
+            'actual_date_time': False,
+            'actual_time': False,
+            'current_time': False,
+            'day_of_month': False,
+            'day_of_week': False,
+            'day_of_year': False,
+            'holiday_index': False,
+            'hour': False,
+            'minutes': False,
+            'month': False,
+            'num_time_steps_in_hour': False,
+            'year': False,
+            'is_raining': False,
+            'sun_is_up': False,
+            'today_weather_albedo_at_time': False,
+            'today_weather_beam_solar_at_time': False,
+            'today_weather_diffuse_solar_at_time': False,
+            'today_weather_horizontal_ir_at_time': False,
+            'today_weather_is_raining_at_time': False,
+            'today_weather_is_snowing_at_time': False,
+            'today_weather_liquid_precipitation_at_time': False,
+            'today_weather_outdoor_barometric_pressure_at_time': False,
+            'today_weather_outdoor_dew_point_at_time': False,
+            'today_weather_outdoor_dry_bulb_at_time': False,
+            'today_weather_outdoor_relative_humidity_at_time': False,
+            'today_weather_sky_temperature_at_time': False,
+            'today_weather_wind_direction_at_time': False,
+            'today_weather_wind_speed_at_time': False,
+            'tomorrow_weather_albedo_at_time': False,
+            'tomorrow_weather_beam_solar_at_time': False,
+            'tomorrow_weather_diffuse_solar_at_time': False,
+            'tomorrow_weather_horizontal_ir_at_time': False,
+            'tomorrow_weather_is_raining_at_time': False,
+            'tomorrow_weather_is_snowing_at_time': False,
+            'tomorrow_weather_liquid_precipitation_at_time': False,
+            'tomorrow_weather_outdoor_barometric_pressure_at_time': False,
+            'tomorrow_weather_outdoor_dew_point_at_time': False,
+            'tomorrow_weather_outdoor_dry_bulb_at_time': False,
+            'tomorrow_weather_outdoor_relative_humidity_at_time': False,
+            'tomorrow_weather_sky_temperature_at_time': False,
+            'tomorrow_weather_wind_direction_at_time': False,
+            'tomorrow_weather_wind_speed_at_time': False,
+        }
+        * zone_simulation_parameters: Dict[str,bool] = {
+            'system_time_step': False,
+            'zone_time_step': False,
+            'zone_time_step_number': False,
+        }
+        
+        * infos_variables: Dict[str,List|Dict[str,List]] = NotImplemented # TODO: add actuators, weather_prediction, building_properties
+        {
+            'variables_env': [],
+            'variables_thz': [],
+            'variables_obj': {'agent_ID': []},
+            'meters': {'agent_ID': []},
+            'static_variables': {'thermal_zone_ID': []},
+            'simulation_parameters': [],
+            'zone_simulation_parameters': []
+        
+        * no_observable_variables: Dict[str,List|Dict[str,List]] = NotImplemented # TODO: add actuators, weather_prediction, building_properties
+        {'variables_env': []
+        'variables_thz': []
+        'variables_obj': {'agent_ID': []}
+        'meters': {'agent_ID': []}
+        'static_variables': {'thermal_zone_ID': []}
+        'simulation_parameters': []
+        'zone_simulation_parameters': []
+        
+        * use_actuator_state: bool = False
+        * use_agent_indicator: bool = True
+        * use_thermal_zone_indicator: bool = False
+        * use_agent_type: bool = False
+        * use_building_properties: bool = False
+        * building_properties: Dict[str,Dict[str,float]] = NotImplemented
+        * use_one_day_weather_prediction: bool = False
+        * prediction_hours: int = 24
+        * prediction_variables: Dict[str,bool] = {
+            'albedo': False,
+            'beam_solar': False,
+            'diffuse_solar': False,
+            'horizontal_ir': False,
+            'is_raining': False,
+            'is_snowing': False,
+            'liquid_precipitation': False,
+            'outdoor_barometric_pressure': False,
+            'outdoor_dew_point': False,
+            'outdoor_dry_bulb': False,
+            'outdoor_relative_humidity': False,
+            'sky_temperature': False,
+            'wind_direction': False,
+            'wind_speed': False,
+        }
+
+        **actions**
+        
+        * action_fn: ActionFunction = NotImplemented
+        * action_fn_config: Dict[str, Any] = {}
+        
+        **rewards**
+        
+        * reward_fn: RewardFunction = NotImplemented
+
+        **functionalities**
+        
+        * cut_episode_len: int = 0
+        * episode_fn: EpisodeFunction = EpisodeFunction({})
+        
         """
         # generals
         self.epjson_path: str = NotImplemented
@@ -31,7 +204,6 @@ class EnvConfig:
         self.output_path: str = NotImplemented
         self.ep_terminal_output: bool = True
         self.timeout: float = 10.0
-        self.evaluation: bool = False
 
         # agents
         self.agents_config: Dict[str,Dict[str,Any]] = NotImplemented
@@ -51,7 +223,7 @@ class EnvConfig:
         # allow EnergyPlus to resume controlling that value, there is an actuator reset function as well.
 
         # observations
-        self.observation_fn: ObservationFunction = ObservationFunction({})
+        self.observation_fn: ObservationFunction = NotImplemented
         self.observation_fn_config: Dict[str, Any] = {}
         
         self.variables_env: List = []
@@ -176,11 +348,11 @@ class EnvConfig:
         }
 
         # actions
-        self.action_space: spaces.Space[ActType] = None
-        self.action_fn: ActionFunction = ActionFunction({})
+        self.action_fn: ActionFunction = NotImplemented
+        self.action_fn_config: Dict[str, Any] = {}
         
         # rewards
-        self.reward_fn: RewardFunction = RewardFunction({})
+        self.reward_fn: RewardFunction = NotImplemented
 
         # functionalities
         self.cut_episode_len: int = 0
@@ -193,7 +365,6 @@ class EnvConfig:
         output_path:str = NotImplemented,
         ep_terminal_output:Optional[bool] = True,
         timeout:Optional[float] = 10.0,
-        evaluation: bool = False,
         ):
         """
         This method is used to modify the general configuration of the environment.
@@ -217,7 +388,6 @@ class EnvConfig:
         self.output_path = output_path
         self.ep_terminal_output = ep_terminal_output
         self.timeout = timeout
-        self.evaluation = evaluation
         
     def agents(
         self,
@@ -369,8 +539,8 @@ class EnvConfig:
     
     def actions(
         self,
-        action_space: spaces.Space[ActType] = None,
-        action_fn: ActionFunction = ActionFunction({}),
+        action_fn: ActionFunction = NotImplemented,
+        action_fn_config: Dict[str, Any] = {},
         ):
         """
         This method is used to modify the actions configuration of the environment.
@@ -385,15 +555,14 @@ class EnvConfig:
             objective to transform appropiately the discret action into a value action for EP we define the action_fn. 
             This function take the arguments agent_id and action. You can find examples in eprllib.ActionFunctions.
         """
-        if action_space != None:
-            self.action_space = action_space
-            assert isinstance(action_space, spaces.Space), "The action_space must be a gym space."
-            
+        if action_fn == NotImplemented:
+            raise NotImplementedError("action_fn must be defined.")
         self.action_fn = action_fn
+        self.action_fn_config = action_fn_config
 
     def rewards(
         self,
-        reward_fn: RewardFunction = RewardFunction({}),
+        reward_fn: RewardFunction = NotImplemented,
         ):
         """
         This method is used to modify the rewards configuration of the environment.
@@ -402,6 +571,8 @@ class EnvConfig:
             reward_fn (RewardFunction): The reward funtion take the arguments EnvObject (the GymEnv class) and the infos 
             dictionary. As a return, gives a float number as reward. See eprllib.RewardFunctions for examples.
         """
+        if reward_fn == NotImplemented:
+            raise NotImplementedError("reward_fn must be defined.")
         self.reward_fn = reward_fn
 
     def functionalities(
