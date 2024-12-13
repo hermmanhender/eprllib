92f6319c5d5331b20f7a7c30ec1a2126a6f74dee
hermmanhender <hendger.07@gmail.com>
2024-12-13 11:18:33 +0100

    1.5.01

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'eprllib'
 copyright = '2024, Germán Rodolfo Henderson'
 author = 'Germán Rodolfo Henderson'
-release = '1.5.0'
+release = '1.5.01'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
