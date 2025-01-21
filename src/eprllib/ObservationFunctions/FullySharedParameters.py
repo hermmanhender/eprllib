52dab8df3506e420e90b5e0e7fc6adaf07867853
hermmanhender <hendger.07@gmail.com>
2025-01-21 22:57:35 +0100

    Update FullySharedParameters.py

--- src/eprllib/ObservationFunctions/FullySharedParameters.py
+++ src/eprllib/ObservationFunctions/FullySharedParameters.py
@@ -15,7 +15,7 @@ this end, a concatenation of variables is performed.
 # TODO: The number of actuators for the case of other agents is variable. Change the actuator state for agent action or see how
 # could be possible to expand the observation space to addap it to different observations size.
 
-
+import time
 from typing import Any, Dict, Tuple
 from eprllib.ObservationFunctions.ObservationFunctions import ObservationFunction
 
@@ -119,9 +119,14 @@ class FullySharedParameters(ObservationFunction):
                 raise ValueError("The total amount of actuators in the environment is greater than the number of actuators in the environment configuration.")
             else:
                 obs_space_len_shared += self.number_of_actuators_total
-                
-        # construct the observation space.
-        return gym.spaces.Box(float("-inf"), float("inf"), (obs_space_len_shared,))
+        
+        return gym.spaces.Dict(
+            {
+                agent: gym.spaces.Box(float("-inf"), float("inf"), (obs_space_len_shared, )) 
+                for agent 
+                in agent_list
+            }
+        )
         
     def set_agent_obs(
         self,
@@ -136,9 +141,11 @@ class FullySharedParameters(ObservationFunction):
         
         id = 0
         if self.actuator_ids is None:
+            self.actuator_ids = {}
             for agent in env_config['agents_config'].keys():
-                self.actuator_ids = {f"{agent}: {actuator_config[0]}: {actuator_config[1]}: {actuator_config[2]}": id for actuator_config in env_config["agents_config"][agent]["action"]["actuators"]}
-                id += 1
+                for actuator_config in env_config["agents_config"][agent]["action"]["actuators"]:
+                    self.actuator_ids.update({f"{agent}: {actuator_config[0]}: {actuator_config[1]}: {actuator_config[2]}": id})
+                    id += 1
         
         # agents in this timestep
         agent_list = [key for key in agent_states.keys()]
@@ -148,6 +155,7 @@ class FullySharedParameters(ObservationFunction):
         if len(agent_list) < self.number_of_agents_total:
             raise ValueError("The number of agents must be greater than the number of agents in the environment configuration.")
         
+        actuator_names = {agent: {} for agent in agent_list}
         
         for agent in agent_list:
             # Agent properties
@@ -162,43 +170,41 @@ class FullySharedParameters(ObservationFunction):
                 agent_id_vector[self.agent_ids[agent]] = 1
                 # 3. Infos: This is not useful for this part of the observation.
             
-            # === Environment state ===
-            # 1. Label: NotImplementd yet.
-            # 2. Values: Transform the observation in a numpy array to meet the condition expected in a RLlib Environment
-            ag_var = np.array(list(agent_states[agent].values()), dtype='float32')
+            # Remove from agent_states and save the actuator items.
+            for actuator_config in env_config["agents_config"][agent]["action"]["actuators"]:
+                actuator_name = f"{agent}: {actuator_config[0]}: {actuator_config[1]}: {actuator_config[2]}"
+                actuator_names[agent].update({actuator_name: agent_states[agent].pop(actuator_name)})
+                
             if agent_id_vector is not None:
-                ag_var = np.concatenate(
+                agents_obs[agent] = np.concatenate(
                     (
                         agent_id_vector,
-                        ag_var
-                    ),
-                    dtype='float32'
-                )
-            # if apply, add the actuator state of this agent
-            use_actuator_state_flag = False
-            for agent2 in agent_list:
-                if env_config["agents_config"][agent2]["observation"]['use_actuator_state']:
-                    use_actuator_state_flag = True
-                    
-            if use_actuator_state_flag:
-                agent_list_copy = agent_list.copy()
-                agent_list_copy.remove(agent)
-                
-                actuator_id_vector = np.array([-2]*self.number_of_actuators_total)
-                
-                for agent3 in agent_list_copy:
-                    for actuator_config in env_config["agents_config"][agent3]["action"]["actuators"]:
-                        actuator_name = f"{agent3}: {actuator_config[0]}: {actuator_config[1]}: {actuator_config[2]}"
-                        actuator_id_vector[self.actuator_ids[actuator_name]] = agent_states[agent3][actuator_name]
-                        
-                ag_var = np.concatenate(
-                    (
-                        ag_var,
-                        actuator_id_vector,
+                        np.array(list(agent_states[agent].values()), dtype='float32')
                     ),
                     dtype='float32'
                 )
             
-            agents_obs[agent] = ag_var
+        # if apply, add the actuator state as a vector of all agents.
+        use_actuator_state_flag = False
+        for agent in agent_list:
+            if env_config["agents_config"][agent]["observation"]['use_actuator_state']:
+                use_actuator_state_flag = True
+                
+        if use_actuator_state_flag:
+            
+            actuator_id_vector = np.array([-2]*self.number_of_actuators_total)
+            
+            for agent in agent_list:
+                for actuator in actuator_names[agent]:
+                    actuator_id_vector[self.actuator_ids[actuator]] = actuator_names[agent][actuator]
+            
+            for agent in agent_list:        
+                agents_obs[agent] = np.concatenate(
+                    (
+                        agents_obs[agent],
+                        actuator_id_vector,
+                    ),
+                    dtype='float32'
+                )
             
         return agents_obs
