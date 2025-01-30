0f6a8e54514ff701da394db1196f4fe45fa2903c
Germán <52546433+hermmanhender@users.noreply.github.com>
2025-01-30 16:47:37 +0100

    Update independent.py

--- src/eprllib/MultiagentFunctions/independent.py
+++ src/eprllib/MultiagentFunctions/independent.py
@@ -91,7 +91,8 @@ class independent(MultiagentFunction):
         env_config: Dict[str, Any],
         agent_states: Dict[str,Dict[str,Any]],
         dict_agents_obs: Dict[str,Any],
-        infos: Dict[str, Dict[str, Any]]
+        infos: Dict[str, Dict[str, Any]],
+        is_last_timestep: bool = False
     ) -> Tuple[Dict[str, Any], Dict[str, Dict[str, Any]], bool]:
         
         # Agents in this timestep
