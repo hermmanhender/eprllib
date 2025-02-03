f7e1d2d117ed19f5197610555691b39a90a72041
hermmanhender <hendger.07@gmail.com>
2025-02-03 20:31:52 +0100

    Update __init__.py

--- src/eprllib/Agents/__init__.py
+++ src/eprllib/Agents/__init__.py
@@ -1,8 +1,22 @@
 """
 Agents
-======
+=======
 
 This module contains classes for representing and manipulating agents in the
 environment. The agents are responsible for taking actions in the environment
 following a specified policy.
+
+In this module, you will find:
+
+- ``AgentSpec``: The main class for defining agents, including their observation, 
+  filter, action, trigger, and reward specifications.
+- ``ObservationSpec``: Defines the observation space for the agent.
+- ``FilterSpec``: Defines filters to preprocess observations before they are fed to 
+  the agent.
+- ``ActionSpec``: Defines the action space and actuators for the agent.
+- ``TriggerSpec``: Defines triggers that determine when the agent should take an action.
+- ``RewardSpec``: Defines the reward function for the agent.
+
+Additionally, you will find base classes and some applications for Filters, Rewards, 
+and Triggers, which are essential parts of an agent in eprllib.
 """
