949f72878bf0cc0a0723fa3050d6d7fd2376e3db
hermmanhender <hendger.07@gmail.com>
2025-02-16 11:22:21 +0100

    Update FullySharedParametersFilter.py

--- src/eprllib/Agents/Filters/FullySharedParametersFilter.py
+++ src/eprllib/Agents/Filters/FullySharedParametersFilter.py
@@ -4,8 +4,8 @@ Fully-shared-parameters policy (Observation Function)
 
 This module contains the filter class for the fully-shared-parameters policy. This filter builds the observation
 vector for the agent using this policy. The main reason to use this filter is to remove the actuator state from
-the agent state vector, as this will be added in the vector of actuator_states in the ``fully_shared_parameters``
-class for ``MultiagentFunctions``.
+the agent state vector, as this will be added in the vector of actuator_states in the ``FullySharedParametersConnector``
+class for ``AgentsConnectors``.
 """
 
 from typing import Any, Dict
@@ -15,12 +15,19 @@ from eprllib.Utils.observation_utils import get_actuator_name
 from eprllib.Utils.annotations import override
 
 class FullySharedParametersFilter(BaseFilter):
+    """
+    Filter class for the fully-shared-parameters policy.
+
+    This filter builds the observation vector for the agent using this policy. The main reason to use this 
+    filter is to remove the actuator state from the agent state vector, as this will be added in the vector 
+    of actuator_states in the ``FullySharedParametersConnector`` class for ``AgentsConnectors``.
+    """
     def __init__(
         self,
         filter_fn_config: Dict[str, Any]
     ):
         """
-        Filter class for the fully-shared-parameters policy.
+        Initializes the FullySharedParametersFilter class.
 
         Args:
             filter_fn_config (Dict[str, Any]): Configuration dictionary for the filter function.
@@ -28,17 +35,17 @@ class FullySharedParametersFilter(BaseFilter):
         super().__init__(filter_fn_config)
     
     @override(BaseFilter)
-    def set_agent_obs(
+    def get_filtered_obs(
         self,
         env_config: Dict[str, Any],
-        agent_states: Dict[str, Any] = NotImplemented,
+        agent_states: Dict[str, Any],
     ) -> Dict[str, Any]:
         """
         Sets the observation for the agent by removing the actuator state from the agent state vector.
 
         Args:
             env_config (Dict[str, Any]): Configuration dictionary for the environment.
-            agent_states (Dict[str, Any], optional): Dictionary containing the states of the agent. Defaults to NotImplemented.
+            agent_states (Dict[str, Any], optional): Dictionary containing the states of the agent.
 
         Returns:
             Dict[str, Any]: Dictionary containing the observations for the agent.
