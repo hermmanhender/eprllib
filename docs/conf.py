ead287d9a9ac493f4a480bda6e7038a6fb07f608
Germán <52546433+hermmanhender@users.noreply.github.com>
2025-04-02 12:24:40 +0200

    v1.5.16

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ project = 'eprllib'
 copyright = '2024, Germán Rodolfo Henderson'
 author = 'Germán Rodolfo Henderson'
 
-release = '1.5.15'
+release = '1.5.16'
 
 
 # -- General configuration ---------------------------------------------------
