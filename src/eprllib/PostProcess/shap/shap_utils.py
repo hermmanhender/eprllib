9bfcbf89f88219ab05a8aaec72c4ce872f22c5ec
Germán <52546433+hermmanhender@users.noreply.github.com>
2025-05-27 15:05:55 +0200

    Update shap_utils.py

--- src/eprllib/PostProcess/shap/shap_utils.py
+++ src/eprllib/PostProcess/shap/shap_utils.py
@@ -4,92 +4,73 @@ SHAP Utilities to analyse policies
 
 
 """
-import pandas as pd
 import torch
 import shap
-from typing import Dict, Any
 from ray.rllib.policy.policy import Policy
-from eprllib.Environment.Environment import Environment
+from ray.rllib.algorithms.algorithm import Algorithm
+import numpy as np
 
-def generate_experience(
-    policy: Policy, 
-    env: Environment, 
-    num_episodes=5):
-    first_episode = True
-    done = {"__all__": False}
-    for _ in range(num_episodes):
-        obs, info = env.reset()
-        _agent_ids = [agent for agent in obs.keys()]
-        
-        if first_episode:
-            all_observations = {agent: [] for agent in _agent_ids}
-            all_actions = {agent: [] for agent in _agent_ids}
-            all_rewards = {agent: [] for agent in _agent_ids}
-            all_dones = {agent: [] for agent in _agent_ids}
-            all_infos = {agent: [] for agent in _agent_ids}
-            first_episode = False
-        
-        action: Dict[str,Any] = {agent: 0 for agent in _agent_ids}
-        done["__all__"] = False
-        episode_observations = {agent: [] for agent in _agent_ids}
-        episode_actions = {agent: [] for agent in _agent_ids}
-        episode_rewards = {agent: [] for agent in _agent_ids}
-        episode_dones = {agent: [] for agent in _agent_ids}
-        episode_infos = {agent: [] for agent in _agent_ids}
 
-        while not done["__all__"]:
-            for agent in _agent_ids:
-                action[agent], _, _ = policy.compute_single_action(obs[agent], explore=False) # No exploration
-               
-            new_obs, reward, done, truncateds, new_info = env.step(action)
-
-            for agent in _agent_ids:
-                episode_observations[agent].append(obs[agent])
-                episode_actions[agent].append(action[agent])
-                episode_rewards[agent].append(reward[agent])
-                episode_dones[agent].append(done["__all__"])
-                episode_infos[agent].append(info[agent])
-
-            obs = new_obs
-            info = new_info
-
-        for agent in _agent_ids:
-            all_observations[agent].extend(episode_observations[agent])
-            all_actions[agent].extend(episode_actions[agent])
-            all_rewards[agent].extend(episode_rewards[agent])
-            all_dones[agent].extend(episode_dones[agent])
-            all_infos[agent].extend(episode_infos[agent])
-
-    df = pd.DataFrame({
-        agent:{
-            "observations": all_observations[agent],
-            "actions": all_actions[agent],
-            "rewards": all_rewards[agent],
-            "dones": all_dones[agent],
-            "infos": all_infos[agent]
-        } for agent in _agent_ids
-    })
-    return df
-
-# Model prediction function (adapted for RLlib)
-def model_predict(
-    data,
-    policy: Policy
+class eprllibSHAP:
+    """
+    eprllibSHAP is a utility class for integrating SHAP with RLlib policies.
+    """
+    def __init__(
+        self,
+        algorithm: Algorithm,
+        policy_name: str
     ):
-    data_tensor = torch.tensor(data, dtype=torch.float32)
-    with torch.no_grad():
-        predictions = policy.compute_actions(obs_batch=data_tensor.numpy(),exploration=False)[0]
-    return predictions
+        """
+        Initialize the eprllibSHAP class with an RLlib algorithm and policy name.
+        
+        Args:
+            algorithm (Algorithm): The RLlib algorithm instance.
+            policy_name (str): The name of the policy to be used for predictions.
+        
+        Raises:
+            ValueError: If the specified policy name is not found in the algorithm.
+        """
+        self.POLICY: Policy = algorithm.get_policy(policy_name)
+        
+        
+    def model_predict(
+        self,
+        data: np.ndarray,
+    ):
+        """
+        Predict actions using the RLlib policy.
+        
+        Args:
+            data (np.ndarray): The input data for prediction.
+        
+        Returns:
+            np.ndarray: The predicted actions.
+        """
+        data_tensor = torch.tensor(data, dtype=torch.float32)
+        with torch.no_grad():
+            predictions = self.POLICY.compute_actions(obs_batch=data_tensor.numpy(),exploration=False)[0]
+        return predictions
 
-def EPExplainer(
-    model_predict,
-    data,
-    feature_names,
-    sample_size:int = None
-):
-    # apply sample data if given
-    if sample_size is not None:
-        data = shap.sample(data, sample_size)
-    
-    shap.KernelExplainer(model_predict, data, feature_names)
-    
+
+    def EPExplainer(
+        self,
+        data,
+        feature_names,
+        sample_size:int = None
+    ) -> shap.KernelExplainer:
+        """
+        Create a SHAP KernelExplainer for the RLlib policy.
+        
+        Args:
+            data (np.ndarray): The input data for the explainer.
+            feature_names (list): List of feature names.
+            sample_size (int, optional): Number of samples to use for the explainer. Defaults to None.
+            
+        Returns:
+            shap.KernelExplainer: The SHAP KernelExplainer.
+        """
+        # apply sample data if given
+        if sample_size is not None:
+            data = shap.sample(data, sample_size)
+        
+        return shap.KernelExplainer(self.model_predict, data, feature_names)
