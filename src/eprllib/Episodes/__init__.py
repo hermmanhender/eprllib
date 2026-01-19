c7e82812002a4637dbba02ee689e3b4868988f5f
Germán <52546433+hermmanhender@users.noreply.github.com>
2026-01-19 19:33:24 +0100

    Update __init__.py

--- src/eprllib/Episodes/__init__.py
+++ src/eprllib/Episodes/__init__.py
@@ -2,5 +2,7 @@
 Episode Functions
 =================
 
-This module contains functions for working with episodes.
+This module contains the methods necesary to modify during runtime the environment for 
+each episode. This allow to program different training steps like requiered by
+curriculum learning.
 """
