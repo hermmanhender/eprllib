7bd4b6bdd46dbcedda42948ce4a616dbf90bf012
Germán <52546433+hermmanhender@users.noreply.github.com>
2026-03-02 22:09:28 +0100

    Update __init__.py

--- src/eprllib/Agents/Rewards/__init__.py
+++ src/eprllib/Agents/Rewards/__init__.py
@@ -9,12 +9,12 @@ This module contains the reward functions used in reinforcement learning and
 implemented in ``eprllib``.
 
 You can implement your own reward function by creating a new class that inherits from
-:class:`~eprllib.Agents.Rewards.BaseReward` and overriding the ``get_reward`` method.
+``eprllib.Agents.Rewards.BaseReward`` and overriding the ``get_reward`` method.
 
 The module includes the following classes:
 
-- :class:`~eprllib.Agents.Rewards.BaseReward`: The base class for creating reward functions.
-- :class:`~eprllib.Agents.Rewards.RewardSpec`: which is used to specify the configuration of reward functions.
+- ``eprllib.Agents.Rewards.BaseReward`` : The base class for creating reward functions.
+- ``eprllib.Agents.Rewards.RewardSpec`` : which is used to specify the configuration of reward functions.
 
 These classes are used in the ``EnvironmentConfig`` class to specify the rewards that can be
 performed on the environment.
