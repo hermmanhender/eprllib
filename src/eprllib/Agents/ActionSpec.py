ed5b5a72e3f1759c2ca976198b973d61b1231578
Germán <52546433+hermmanhender@users.noreply.github.com>
2026-03-02 22:08:58 +0100

    Update ActionSpec.py

--- src/eprllib/Agents/ActionSpec.py
+++ src/eprllib/Agents/ActionSpec.py
@@ -23,31 +23,35 @@ class ActionSpec:
         Construction method.
         
         Args:
+        
             actuators (Dict[str, Tuple[str, str, str]]): Actuators are the way that users modify the program at 
-            runtime using custom logic and calculations. Not every variable inside EnergyPlus can be 
-            actuated. This is intentional, because opening that door could allow the program to run at 
-            unrealistic conditions, with flow imbalances or energy imbalances, and many other possible problems.
-            Instead, a specific set of items are available to actuate, primarily control functions, 
-            flow requests, and environmental boundary conditions. These actuators, when used in conjunction 
-            with the runtime API and data exchange variables, allow a user to read data, make decisions and 
-            perform calculations, then actuate control strategies for subsequent time steps.
-            Actuator functions are similar, but not exactly the same, as for variables. An actuator
-            handle/ID is still looked up, but it takes the actuator type, component name, and control
-            type, since components may have more than one control type available for actuation. The
-            actuator can then be “actuated” by calling a set-value function, which overrides an internal
-            value, and informs EnergyPlus that this value is currently being externally controlled. To
-            allow EnergyPlus to resume controlling that value, there is an actuator reset function as well.
-            One agent can manage several actuators.
+                runtime using custom logic and calculations. Not every variable inside EnergyPlus can be 
+                actuated. This is intentional, because opening that door could allow the program to run at 
+                unrealistic conditions, with flow imbalances or energy imbalances, and many other possible problems.
+                Instead, a specific set of items are available to actuate, primarily control functions, 
+                flow requests, and environmental boundary conditions. These actuators, when used in conjunction 
+                with the runtime API and data exchange variables, allow a user to read data, make decisions and 
+                perform calculations, then actuate control strategies for subsequent time steps.
+                Actuator functions are similar, but not exactly the same, as for variables. An actuator
+                handle/ID is still looked up, but it takes the actuator type, component name, and control
+                type, since components may have more than one control type available for actuation. The
+                actuator can then be “actuated” by calling a set-value function, which overrides an internal
+                value, and informs EnergyPlus that this value is currently being externally controlled. To
+                allow EnergyPlus to resume controlling that value, there is an actuator reset function as well.
+                One agent can manage several actuators.
         
         Raises:
+        
             ValueError: If the actuators are not defined as a list of tuples of 3 elements.
             KeyError: If an invalid key is provided when setting an item.
             
         """
         self.actuators = actuators
+        
     
     def __getitem__(self, key: str) -> Any:
         return getattr(self, key)
+    
 
     def __setitem__(self, key: str, value: Any) -> None:
         valid_keys = self.__dict__.keys()
@@ -57,6 +61,7 @@ class ActionSpec:
             raise KeyError(msg)
         setattr(self, key, value)
         
+        
     def build(self) -> Dict[str, Any]:
         """
         This method is used to build the ActionSpec object.
@@ -65,7 +70,7 @@ class ActionSpec:
             logger.info("ActionSpec: No actuators provided.")
             self.actuators = {}
         
-        if not isinstance(self.actuators, dict):
+        if not isinstance(self.actuators, dict): # type: ignore
             msg = f"ActionSpec: The actuators must be defined as a dict with actuator names as keys and tuples of 3 elements but {type(self.actuators)} was given."
             logger.error(msg)
             raise ValueError(msg)
