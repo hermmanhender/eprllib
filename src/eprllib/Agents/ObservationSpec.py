38a650466371283a0471c2e330d8e944f55f340b
Germán <52546433+hermmanhender@users.noreply.github.com>
2026-03-02 22:09:24 +0100

    Update ObservationSpec.py

--- src/eprllib/Agents/ObservationSpec.py
+++ src/eprllib/Agents/ObservationSpec.py
@@ -66,77 +66,74 @@ class ObservationSpec:
         Construction method.
         
         Args:
+        
             variables (List[Tuple[str, str]]): Variables represent time series output variables in the simulation. There are thousands
-            of variables made available based on the specific configuration. A user typically requests
-            variables to be in their output files by adding Output:Variable objects to the input file. It
-            is important to note that if the user does not request these variables, they are not tracked,
-            and thus not available on the API.
-            
+                of variables made available based on the specific configuration. A user typically requests
+                variables to be in their output files by adding Output:Variable objects to the input file. It
+                is important to note that if the user does not request these variables, they are not tracked,
+                and thus not available on the API.
             internal_variables (List[str]): Internal variables form a category of built-in data accessible for eprllib.
-            They are internal in that they access information about the input file from inside EnergyPlus. Internal variables 
-            are automatically made available. The EDD file lists the specific internal variable types, their unique
-            identifying names, and the units. The rest of this section provides information about specific internal variables.
-            To see the EDD file, run the simulation to generate the output files and configure an object in Output:EnergyManagementSystem
-            with "Internal Variable Availability Dictionary Reporting" to Verbose.
-            
+                They are internal in that they access information about the input file from inside EnergyPlus. Internal variables 
+                are automatically made available. The EDD file lists the specific internal variable types, their unique
+                identifying names, and the units. The rest of this section provides information about specific internal variables.
+                To see the EDD file, run the simulation to generate the output files and configure an object in Output:EnergyManagementSystem
+                with "Internal Variable Availability Dictionary Reporting" to Verbose.
             meters (List[str]): Meters represent groups of variables which are collected together, much like a meter on
-            a building which represents multiple energy sources. Meters are handled the same way as
-            variables, except that meters do not need to be requested prior running a simulation. From
-            an API standpoint, a client must simply get a handle to a meter by name, and then access
-            the meter value by using a get-value function on the API.
-            
+                a building which represents multiple energy sources. Meters are handled the same way as
+                variables, except that meters do not need to be requested prior running a simulation. From
+                an API standpoint, a client must simply get a handle to a meter by name, and then access
+                the meter value by using a get-value function on the API.
             simulation_parameters (Dict[str, bool]): A number of parameters are made available as they vary through the
-            simulation, including the current simulation day of week, day of year, hour, and many other
-            things. These do not require a handle, but are available through direct function calls. The keys (all by default False) are:        
-                'actual_date_time', 'actual_time', 'current_time', 'day_of_month', 'day_of_week', 'day_of_year', 'holiday_index',
-                'hour', 'minutes', 'month', 'num_time_steps_in_hour', 'year', 'is_raining', 'sun_is_up', 'today_weather_albedo_at_time',
-                'today_weather_beam_solar_at_time', 'today_weather_diffuse_solar_at_time', 'today_weather_horizontal_ir_at_time',
-                'today_weather_is_raining_at_time', 'today_weather_is_snowing_at_time', 'today_weather_liquid_precipitation_at_time',
-                'today_weather_outdoor_barometric_pressure_at_time', 'today_weather_outdoor_dew_point_at_time', 
-                'today_weather_outdoor_dry_bulb_at_time', 'today_weather_outdoor_relative_humidity_at_time', 'today_weather_sky_temperature_at_time',
-                'today_weather_wind_direction_at_time', 'today_weather_wind_speed_at_time', 'tomorrow_weather_albedo_at_time',
-                'tomorrow_weather_beam_solar_at_time', 'tomorrow_weather_diffuse_solar_at_time', 'tomorrow_weather_horizontal_ir_at_time',
-                'tomorrow_weather_is_raining_at_time', 'tomorrow_weather_is_snowing_at_time', 'tomorrow_weather_liquid_precipitation_at_time',
-                'tomorrow_weather_outdoor_barometric_pressure_at_time', 'tomorrow_weather_outdoor_dew_point_at_time', 'tomorrow_weather_outdoor_dry_bulb_at_time',
-                'tomorrow_weather_outdoor_relative_humidity_at_time', 'tomorrow_weather_sky_temperature_at_time', 'tomorrow_weather_wind_direction_at_time',
-                'tomorrow_weather_wind_speed_at_time'
+                simulation, including the current simulation day of week, day of year, hour, and many other
+                things. These do not require a handle, but are available through direct function calls. The keys (all by default False) are:  
+                    
+                    'actual_date_time', 'actual_time', 'current_time', 'day_of_month', 'day_of_week', 'day_of_year', 'holiday_index',
+                    'hour', 'minutes', 'month', 'num_time_steps_in_hour', 'year', 'is_raining', 'sun_is_up', 'today_weather_albedo_at_time',
+                    'today_weather_beam_solar_at_time', 'today_weather_diffuse_solar_at_time', 'today_weather_horizontal_ir_at_time',
+                    'today_weather_is_raining_at_time', 'today_weather_is_snowing_at_time', 'today_weather_liquid_precipitation_at_time',
+                    'today_weather_outdoor_barometric_pressure_at_time', 'today_weather_outdoor_dew_point_at_time', 
+                    'today_weather_outdoor_dry_bulb_at_time', 'today_weather_outdoor_relative_humidity_at_time', 'today_weather_sky_temperature_at_time',
+                    'today_weather_wind_direction_at_time', 'today_weather_wind_speed_at_time', 'tomorrow_weather_albedo_at_time',
+                    'tomorrow_weather_beam_solar_at_time', 'tomorrow_weather_diffuse_solar_at_time', 'tomorrow_weather_horizontal_ir_at_time',
+                    'tomorrow_weather_is_raining_at_time', 'tomorrow_weather_is_snowing_at_time', 'tomorrow_weather_liquid_precipitation_at_time',
+                    'tomorrow_weather_outdoor_barometric_pressure_at_time', 'tomorrow_weather_outdoor_dew_point_at_time', 'tomorrow_weather_outdoor_dry_bulb_at_time',
+                    'tomorrow_weather_outdoor_relative_humidity_at_time', 'tomorrow_weather_sky_temperature_at_time', 'tomorrow_weather_wind_direction_at_time',
+                    'tomorrow_weather_wind_speed_at_time'
             
             zone_simulation_parameters (Dict[str, bool]):  A number of parameters are made available as they vary through the
-            simulation for thermal zones. The keys (all by default False) are:
-                'system_time_step', 'zone_time_step', 'zone_time_step_number'
-            
+                simulation for thermal zones. The keys (all by default False) are:
+                
+                    'system_time_step', 'zone_time_step', 'zone_time_step_number'
+                
             use_one_day_weather_prediction (bool): We use the internal variables of EnergyPlus to provide with a 
             prediction of the weather time ahead. You can specify the `weather_prediction_hours` and the prediction variables
             listed on `prediction_variables`.
-            
             weather_prediction_hours (int): Default is 24
-            
             prediction_variables (Dict[str, bool]): See `use_one_day_weather_prediction`. The variables are (by default 
-            all False):
-                'albedo', 'beam_solar', 'diffuse_solar', 'horizontal_ir', 'is_raining', 'is_snowing', 'liquid_precipitation',
-                'outdoor_barometric_pressure', 'outdoor_dew_point', 'outdoor_dry_bulb', 'outdoor_relative_humidity',
-                'sky_temperature', 'wind_direction', 'wind_speed'.
+                all False):
             
+                    'albedo', 'beam_solar', 'diffuse_solar', 'horizontal_ir', 'is_raining', 'is_snowing', 'liquid_precipitation',
+                    'outdoor_barometric_pressure', 'outdoor_dew_point', 'outdoor_dry_bulb', 'outdoor_relative_humidity',
+                    'sky_temperature', 'wind_direction', 'wind_speed'.
+                
             use_actuator_state (bool): Define if the actuator state will be used as an observation for the agent.
-            
             other_obs (Dict[str, float | int]): Custom observation dictionary.
-            
             user_occupation_function (bool): Define if the user occupation function will be used. Default is False.
-            
             user_occupation_forecast (bool): Define if the user occupation forecast will be used. Default is False.
-            
             user_type (str): Type of user. Default is 'Residential'
-            
             zone_type (str): Type of zone. Default is 'Office'
-            
             occupation_schedule (Tuple[str, str, str]): Occupation schedule for the user occupation forecast. Default is None.
-            
             occupation_prediction_hours (int): Occupation prediction hours for the user occupation forecast. Default is 24.
-            
             confidence_level (float): Confidence level for the user occupation forecast. Default is 0.95.
-            
             lambdaa (float): Decay value of the confidence level. Default is 0.05.
 
+        Raises:
+        
+            ValueError: If the user type or zone type is not valid, if the weather prediction hours or occupation 
+                prediction hours are not between 1 and 24, if the occupation schedule is not provided when user 
+                occupation forecast is True, or if no variable/meter/actuator/parameter is defined in the observation.
+            KeyError: If an invalid key is provided when setting an item.
+            
         """
         # Variables
         self.variables = variables
