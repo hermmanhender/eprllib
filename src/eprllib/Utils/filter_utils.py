f8a2b7542786f252438cb4282499c09d6baccc64
Germán <52546433+hermmanhender@users.noreply.github.com>
2026-03-20 12:39:09 +0100

    Update filter_utils.py

--- src/eprllib/Utils/filter_utils.py
+++ src/eprllib/Utils/filter_utils.py
@@ -2,15 +2,19 @@
 Filter Utilities
 ===================
 
-Work in progress...
+Filters often involve normalization works. Here you can find the most 
+common used normalization functions: sin and cos for Cyclical Feature Encoding, and
+min_max for linear variables. Also here are provided the functions to get 
+the original values previously normalized.
+
 """
 
 import numpy as np
 
 def to_sin_transformation(
     value: float,
-    min: float,
-    max: float
+    min_val: float,
+    max_val: float
     ) -> float:
     """
     Transform a value to a sin function.
@@ -23,7 +27,62 @@ def to_sin_transformation(
     Returns:
         float: The transformed value.
     """
-    return np.sin(2 * np.pi * (value - min) / (max - min))
+    return np.sin(2 * np.pi * (value - min_val) / (max_val - min_val))
+
+def to_cos_transformation(
+    value: float,
+    min_val: float,
+    max_val: float
+    ) -> float:
+    """
+    Transform a value to a sin function.
+    
+    Args:
+        value (float): The value to transform.
+        min (float): The minimum value of the range.
+        max (float): The maximum value of the range.
+        
+    Returns:
+        float: The transformed value.
+    """
+    return np.cos(2 * np.pi * (value - min_val) / (max_val - min_val))
+
+import numpy as np
+
+def from_sin_cos_normalization(
+    value_sin: float, 
+    value_cos: float, 
+    min_val: float|int, 
+    max_val: float|int
+    ) -> float:
+    """
+    Get the original value normalized with sin and cos.
+    
+    Args:
+        value_sin (float): The sin transformed value.
+        value_cos (float): The cos transformed value.
+        min_val (float|int): The minimum value of the range.
+        max_val (float|int): The maximum value of the range.
+    
+    Returns:
+        float: The original value.
+    """
+    # 1. Calcular el rango original
+    range = max_val - min_val
+    
+    # 2. Obtener el ángulo en radianes usando el arco tangente de dos parámetros
+    # arctan2 gestiona automáticamente los signos para darnos el cuadrante correcto
+    angulo = np.arctan2(value_sin, value_cos)
+    
+    # 3. Normalizar el ángulo al rango [0, 1]
+    # np.mod(..., 2*pi) asegura que siempre sea positivo entre 0 y 2pi
+    v_norm = np.mod(angulo, 2 * np.pi) / (2 * np.pi)
+    
+    # 4. Escalar al rango original y sumar el desplazamiento (offset)
+    value = (v_norm * range) + min_val
+    
+    return value
+
 
 def normalization_minmax(
     value: float, 
